bdb877439c2787cd03613ef33ea76658d1b9ea25
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-09 21:02:05 +0600

    fixed the bug

--- app/app.js
+++ app/app.js
@@ -1,16 +1,20 @@
+// Global variable
 const panels = document.querySelectorAll(".panel");
 
+// Toggle the "open" class
 function toggleOpen() {
   console.log("hello!");
   this.classList.toggle("open");
 }
 
+// Toggle the "open-active" class
 function toggleOpenActive(e) {
   if (e.propertyName.includes("flex")) {
     this.classList.toggle("open-active");
   }
 }
 
+// Event handler
 panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
 
 panels.forEach((panel) =>
